b0c074e458f391dbe73e719f3d19b4d7339cb2c2
Willian Fernandes <willian@willianfernandes.com.br>
2022-10-06 09:25:53 -0300

    Update Copilot config

--- lua/nvim/plugins/copilot.lua
+++ lua/nvim/plugins/copilot.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-parameter
 -- For copilot.vim
 -- vim.g.copilot_filetypes = {
 --   ["*"] = false,
@@ -21,6 +22,10 @@ copilot.setup({
   panel = { -- no config options yet
     enabled = true,
   },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+  },
   ft_disable = { 'markdown' },
   -- plugin_manager_path = vim.fn.stdpath "data" .. "/site/pack/packer",
   server_opts_overrides = {
@@ -32,4 +37,6 @@ copilot.setup({
       },
     },
   },
+  copilot_node_command = vim.fn.expand('$HOME') .. '/.asdf/installs/nodejs/lts/bin/node',
+  plugin_manager_path = vim.fn.stdpath('data') .. '/site/pack/packer',
 })
