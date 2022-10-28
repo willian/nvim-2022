9ad1cacd34077b9ab3f61d6f0aef71e176d081af
Willian Fernandes <willian@willianfernandes.com.br>
2022-10-28 07:53:51 -0300

    Improve support to transparent background

--- lua/willian/core/colorscheme.lua
+++ lua/willian/core/colorscheme.lua
@@ -1,6 +1,22 @@
-local status_ok, _ = pcall(vim.cmd, 'colorscheme tokyonight-storm')
+local status_ok, _ = pcall(require, 'tokyonight')
 if not status_ok then
-  -- print error if colorscheme not installed
+  return
+end
+
+local colorscheme_status_ok, _ = pcall(vim.cmd, 'colorscheme tokyonight-storm')
+if not colorscheme_status_ok then
   print('Colorscheme not found!')
   return
 end
+
+local colors = require('tokyonight.colors').setup()
+
+-- tokyonight.setup({
+--   on_highlights = function(hl, colors)
+--     hl.ColorColumn = { bg = colors.red }
+--     hl.Comment = { fg = colors.red }
+--   end,
+-- })
+
+vim.api.nvim_command('highlight ColorColumn guibg=' .. colors.bg_highlight)
+vim.api.nvim_command('highlight CursorLineNr guibg=' .. colors.bg_highlight)
--- lua/willian/plugins/transparent.lua
+++ lua/willian/plugins/transparent.lua
@@ -4,18 +4,10 @@ if not status_ok then
 end
 
 transparent.setup({
-  enable = true, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
-
-    -- example of akinsho/nvim-bufferline.lua
-    -- 'BufferLineTabClose',
-    -- 'BufferlineBufferSelected',
-    -- 'BufferLineFill',
-    -- 'BufferLineBackground',
-    -- 'BufferLineSeparator',
-    -- 'BufferLineIndicatorSelected',
-    -- 'ColorColumn',
+  enable = true,
+  extra_groups = {},
+  exclude = {
+    'LineNr',
+    'CursorLineNr',
   },
-  exclude = {}, -- table: groups you don't want to clear
 })
