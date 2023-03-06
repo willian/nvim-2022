018934b375b40578a765c2f3f80578c7b1ea5131
Willian Fernandes <willian@willianfernandes.com.br>
2023-03-06 12:08:21 -0300

    Add LSP servers and configuration
    Also improve autocompltion with LSP support

--- lua/willian/plugins/lsp.lua
+++ lua/willian/plugins/lsp.lua
@@ -1,3 +1,6 @@
+---@diagnostic disable-next-line: deprecated
+table.unpack = table.unpack or unpack
+
 return {
   -- Autocompletion
   {
@@ -11,17 +14,21 @@ return {
       { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
       'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets',
+
+      -- vs-code like icons for autocompletio
+      'onsails/lspkind.nvim',
     },
     config = function()
       local cmp = require('cmp')
       local luasnip = require('luasnip')
+      local lspkind = require('lspkind')
 
       -- load friendly-snippets
       require("luasnip.loaders.from_vscode").lazy_load()
 
       -- helper function for super tab functionality
       local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0
           and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
       end
@@ -68,11 +75,169 @@ return {
 
         -- sources for autocompletion
         sources = cmp.config.sources({
+          { name = 'nvim_lsp' }, -- lsp
           { name = 'luasnip' }, -- snippets
           { name = 'buffer' }, -- text within current buffer
           { name = 'path' }, -- file system paths
         }),
+
+        -- configure lspkind for vs-code like icons
+        formatting = {
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = '...',
+          }),
+        },
       })
+    end,
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- managing & installing lsp servers, linters, and formatters
+      'williamboman/mason.nvim', -- in charge of managing lsp servers, linters & formatters
+      'williamboman/mason-lspconfig.nvim', -- bridges gap b/w mason & lspconfig
+
+      -- configuring lsp servers
+      'hrsh7th/cmp-nvim-lsp', -- for autocompletion
+      'glepnir/lspsaga.nvim', -- enhanced lsp uis
+      'jose-elias-alvarez/typescript.nvim', -- additional functionality for typescript server (e.g. rename file & update imports)
+      -- 'j-hui/fidget.nvim', -- UI for nvim-lsp progress
+      -- 'MunifTanjim/prettier.nvim', -- prettier plugin
+    },
+    config = function()
+      local lsp_servers = {
+        'cssls',
+        'cssmodules_ls',
+        'dockerls',
+        'emmet_ls',
+        'eslint',
+        'graphql',
+        'html',
+        'jsonls',
+        'lemminx',
+        'lua_ls',
+        'prismals',
+        'pyright',
+        'solargraph',
+        'tailwindcss',
+        'taplo',
+        'tsserver',
+        'yamlls',
+      }
+
+      -- Mason config
+      local mason = require('mason')
+      local mason_lspconfig = require('mason-lspconfig')
+
+      mason.setup()
+
+      mason_lspconfig.setup({
+        -- list of servers for mason to install
+        ensure_installed = lsp_servers,
+        -- auto-install configured servers (with lspconfig)
+        automatic_installation = true, -- not the same as ensure_installed
+      })
+
+      -- Lspsaga
+      local saga = require('lspsaga')
+
+      saga.setup({
+        -- keybinds for navigation in lspsaga window
+        move_in_saga = { prev = '<Up>', next = '<Down>' },
+        -- use enter to open file with finder
+        finder_action_keys = {
+          open = '<CR>',
+        },
+        -- use enter to open file with definition preview
+        definition_action_keys = {
+          edit = '<CR>',
+        },
+      })
+
+      -- LSPConfig
+      local lspconfig = require('lspconfig')
+      local cmp_nvim_lsp = require('cmp_nvim_lsp')
+      local typescript = require('typescript')
+
+      local on_attach = function(client, bufnr)
+        local nmap = function(keys, func, desc)
+          if desc then
+            desc = 'LSP: ' .. desc
+          end
+
+          local opts = { noremap = true, silent = true, buffer = bufnr, desc = desc }
+
+          vim.keymap.set('n', keys, func, opts)
+        end
+
+        -- set keybinds
+        nmap('gf', '<cmd>Lspsaga lsp_finder<CR>', 'References') -- show definition, references
+        nmap('gD', vim.lsp.buf.declaration, 'Goto Declaration') -- got to declaration
+        nmap('gd', '<cmd>Lspsaga goto_definition<CR>', 'Goto Definition') -- see definition and make edits in window
+        nmap('gi', vim.lsp.buf.implementation, 'Goto Implementation') -- go to implementation
+        nmap('[e', '<cmd>Lspsaga diagnostic_jump_prev<CR>', 'Next Error') -- jump to previous diagnostic in buffer
+        nmap(']e', '<cmd>Lspsaga diagnostic_jump_next<CR>', 'Prev Error') -- jump to next diagnostic in buffer
+        nmap('K', '<cmd>Lspsaga hover_doc<CR>', 'Show documentation') -- show documentation for what is under cursor
+        nmap('<leader>D', '<cmd>Lspsaga show_cursor_diagnostics<CR>', 'Cursor Diagnostic') -- show diagnostics for cursor
+        nmap('<leader>d', '<cmd>Lspsaga show_line_diagnostics<CR>', 'Line Diagnostics') -- show  diagnostics for line
+        nmap('<leader>ca', '<cmd>Lspsaga code_action<CR>', 'Code Action') -- see available code actions
+        nmap('<leader>cr', '<cmd>Lspsaga rename<CR>', 'Rename')
+        nmap('<leader>lD', '<cmd>Lspsaga show_cursor_diagnostics<CR>', 'Cursor Diagnostic') -- show diagnostics for cursor
+        nmap('<leader>la', '<cmd>Lspsaga code_action<CR>', 'Code Action')
+        nmap('<leader>ld', '<cmd>Lspsaga show_line_diagnostics<CR>', 'Line Diagnostics') -- show  diagnostics for line
+        nmap('<leader>lr', '<cmd>Lspsaga rename<CR>', 'Rename')
+
+        -- typescript specific keymaps (e.g. rename file and update imports)
+        if client.name == 'tsserver' then
+          nmap('<leader>rf', ':TypescriptRenameFile<CR>', '[TS] Rename File') -- rename file and update imports
+          nmap('<leader>oi', ':TypescriptOrganizeImports<CR>', '[TS] Organize Imports') -- organize imports (not in youtube nvim video)
+          nmap('<leader>ru', ':TypescriptRemoveUnused<CR>', '[TS] Remove Unused') -- remove unused variables (not in youtube nvim video)
+        end
+      end
+
+      -- used to enable autocompletion (assign to every lsp server config)
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+
+      for _, server in pairs(lsp_servers) do
+        if server == 'tsserver' then
+          -- configure typescript server
+          typescript.setup({
+            server = {
+              capabilities = capabilities,
+              on_attach = on_attach,
+            },
+          })
+        elseif server == 'lua_ls' then
+          -- configure lua server (with special settings)
+          lspconfig[server].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = { -- custom settings for lua
+              Lua = {
+                -- make the language server recognize 'vim' global
+                diagnostics = {
+                  globals = { 'vim' },
+                },
+                workspace = {
+                  -- make language server aware of runtime files
+                  library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.stdpath('config') .. '/lua'] = true,
+                  },
+                },
+              },
+            },
+          })
+        else
+          lspconfig[server].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+          })
+        end
+      end
     end,
   },
 }
