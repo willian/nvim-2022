6c2ad4daf62d5ad13c55f1dcf2e6860ac8d3ba7e
Willian Fernandes <willian@Willians-MacBook-Pro-3.local>
2023-03-30 11:26:13 -0300

    Load Treesitter asap

--- lua/willian/plugins/treesitter.lua
+++ lua/willian/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   version = false, -- last release is way too old and doesn't work on Windows
   build = ':TSUpdate',
   event = { 'BufReadPost', 'BufNewFile' },
+  lazy = false,
   dependencies = {
     {
       'nvim-treesitter/nvim-treesitter-textobjects',
