eed5e8069f6709cc0efe502d825c753658fb7654
Willian Fernandes <willian@willianfernandes.com.br>
2023-04-28 06:47:52 -0300

    Improve the colorscheme config

--- lua/willian/plugins/colorscheme.lua
+++ lua/willian/plugins/colorscheme.lua
@@ -1,11 +1,32 @@
+-- colorscheme
 return {
+  -- {
+  --   'bluz71/vim-nightfly-guicolors',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme('nightfly')
+  --     vim.g.nightflyTransparent = false
+  --     vim.g.nightflyItalics = true
+  --     vim.g.nightflyNormalFloat = true
+  --     vim.g.nightflyVirtualTextColor = true
+  --     vim.g.nightflyWinSeparator = 2
+  --
+  --     vim.opt.fillchars = {
+  --       horiz = '━',
+  --       horizup = '┻',
+  --       horizdown = '┳',
+  --       vert = '┃',
+  --       vertleft = '┫',
+  --       vertright = '┣',
+  --       verthoriz = '╋',
+  --     }
+  --   end,
+  -- },
   {
-    -- colorscheme
-    'bluz71/vim-nightfly-guicolors',
-    priority = 1000,
+    'catppuccin/nvim',
+    name = 'catppuccin',
     config = function()
-      vim.cmd.colorscheme('nightfly')
-      vim.g.nightflyTransparent = false
+      vim.cmd.colorscheme('catppuccin')
     end,
   },
 }
